0843dc0e9fdf1a63b943368453c4b4071500b929
In-HyeokJang <dlsgur1261@naver.com>
2022-07-13 17:09:28 +0900

    code:Update

---
+++ final_project/Rent-Car-JIH/Rent-Car-JIH/src/main/java/com/rentcar/carinfo/model/CarConditionDTO.java
@@ -0,0 +1,20 @@
+package com.rentcar.carinfo.model;
+
+import lombok.Data;
+
+@Data
+public class CarConditionDTO {
+    private String wheel ;
+    private String windows ;
+    private String bumper ;
+    private String sideMirror ;
+    private String cardoor ;
+    private String airconditioner ;
+    private String safetyBelt ;
+    private String bluetooth ;
+    private String rearSensor ;
+    private String rearCamera ;
+    private String sunroof ;
+    private String blackbox ;
+    private String carnumber ;
+}

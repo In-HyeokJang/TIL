a24c1415876d3e7cc091538140e4e4657ec8833b
In-HyeokJang <dlsgur1261@naver.com>
2022-10-14 17:40:53 +0900

    nomard:js

--- JavaScript/momentum/app.js
+++ JavaScript/momentum/app.js
@@ -37,6 +37,42 @@
 // undefined 메모리 값은 있지만 비어 있다는 뜻
 // console.log(something, amIFat);
 
+// Arrays(배열)
+// const mon ="mon";
+// const tue = "tue";
+// const wed = "wed";
+// const thu = "thu";
+// const fri = "fri";
+// const sat = "sat";
+// const sun = "sun";
+
+// const days0fWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// // const nonsense = [1, 2, "hello" , false, null, true, undefined, "ethan"]
+
+// // console.log(days0fWeek, nonsense);
+// // array []안 0부터 시작 
+
+// // console.log(days0fWeek[4]);
+
+// // Get Item fram Array
+// console.log(days0fWeek);
+
+// // Add on more day to the array
+// // 배열에 추가 하는 법
+// days0fWeek.push("sun");
+
+// console.log(days0fWeek);
+
+const toBuy = ["ptoato", "tomato", "pizza"];
+toBuy.push("kimbab");
+console.log(toBuy[2]);
+console.log(toBuy[123456]); //undefined
+console.log(toBuy);
+
+
+
+
 
 
 

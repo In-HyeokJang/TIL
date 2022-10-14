d50def24362b15218d8c669791243b9bd02d0b11
In-HyeokJang <dlsgur1261@naver.com>
2022-10-14 17:21:15 +0900

    nomadcoders

---
+++ CloneCoding/momentum/app.js
@@ -0,0 +1,44 @@
+// alert("hi");
+// hello;
+// "hello " + "my name is Ethan";
+// console.log(5454545454);
+// console.log("lalalalal");
+// console.log('asasfa');
+// console.log(5+2);
+// console.log(5*2);
+// console.log(5/2);
+
+
+// const : (상수, variable) 변함 없는 값 
+// let : (상수, variable) 값을 변경 할 수 있음
+// var : 언제든지 업데이트 가능 (뭐에 사용하는지 잘 알 수 없음 그래서 잘 사용 x)
+// const a = 5;
+// let b = 2;
+// var myName = "Ethan";
+
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log("hello"+ myName);
+
+// b =3 
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+
+// myName = "EthanJang";
+// console.log("your name " + myName);
+
+// Boolean
+// true or false or null
+// null 값이 없는 것으로 채워졌다 뜻
+// const amIFat = null; 
+// let something ; //undefined
+// undefined 메모리 값은 있지만 비어 있다는 뜻
+// console.log(something, amIFat);
+
+
+
+
+
+

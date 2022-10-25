cf6974863e4571491277c16a4e3bbe7b02ac9923
In-HyeokJang <dlsgur1261@naver.com>
2022-10-25 14:15:21 +0900

    javascript

--- JavaScript/momentum/app.js
+++ JavaScript/momentum/app.js
@@ -235,7 +235,50 @@
 // calculator.multi(1,2);
 // calculator.power(1,5);
 
+// Returns
 
+const calculator = {
+    add : function (a, b){
+        return a+b;
+    },
+    minus : function(a,b){
+        return a-b;
+    },
+    div : function(a,b){
+        return a/b;
+    },
+    multi : function(a,b){
+        return a*b;
+    },
+    power : function (a, b) {
+        return a**b;
+    }
+};
+
+const plusResult = calculator.add(2,3);
+const minusResult = calculator.minus(plusResult,10);
+const divResult = calculator.div(10,minusResult);
+const multiResult = calculator.multi(divResult,plusResult);
+const powerResult = calculator.power(divResult,minusResult);
+
+// // calculator.add(5, 1);
+// // calculator.minus(10,2);
+// // calculator.div(10,5);
+// // calculator.multi(1,2);
+// // calculator.power(1,5);
+
+// console.log(calculator.add(2,3));
+
+
+const age = 95;
+function calculateKrAge(ageOfForeigner){ //95
+    ageOfForeigner + 2;  // return 95 + 2
+    return "hello";
+}
+
+const krAge = calculateKrAge(age); // 95
+
+console.log(krAge); // 97 //hello
 
 
 
